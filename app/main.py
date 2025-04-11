3f6fa3f8775171b271bf25f38b8959558639ed46
Eric Garza Gonzalez <ericgarzaroot@gmail.com>
2025-04-11 13:43:22 -0600

    Add CORS

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import api
 from .services.db_service import engine
 from sqlmodel import SQLModel
 
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 SQLModel.metadata.create_all(engine)
 
